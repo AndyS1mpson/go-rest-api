07fce38c7f13d8c117b8c5ed9cc032e9a1f08137
Andrey Ageev <whale942999@gmail.com>
2023-03-23 20:47:14 +0300

    refactoring

--- internal/app/store/sqlstore/user_repository.go
+++ internal/app/store/sqlstore/user_repository.go
@@ -1,6 +1,11 @@
-package store
+package sqlstore
 
-import "github.com/AndyS1mpson/go-rest-api/internal/app/model"
+import (
+	"database/sql"
+
+	"github.com/AndyS1mpson/go-rest-api/internal/app/model"
+	"github.com/AndyS1mpson/go-rest-api/internal/app/store"
+)
 
 // UserRepository ...
 type UserRepository struct {
@@ -8,24 +13,20 @@ type UserRepository struct {
 }
 
 // Create ...
-func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+func (r *UserRepository) Create(u *model.User) error {
 	if err := u.Validate(); err != nil {
-		return nil, err
+		return err
 	}
 
 	if err := u.BeforeCreate(); err != nil {
-		return nil, err
+		return err
 	}
 
-	if err := r.store.db.QueryRow(
+	return r.store.db.QueryRow(
 		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
 		u.Email,
 		u.EncryptedPassword,
-	).Scan(&u.ID); err != nil {
-		return nil, err
-	}
-
-	return u, nil
+	).Scan(&u.ID)
 }
 
 // FindByEmail ...
@@ -38,8 +39,8 @@ func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
 		&u.ID,
 		&u.Email,
 		&u.EncryptedPassword,
-	); err != nil {
-		return nil, err
+	); err == sql.ErrNoRows {
+		return nil, store.ErrRecordNotFound
 	}
 
 	return u, nil
